81a2d5a85105b95c261dcae533074f98c7ee85b4
JOSnow985 <jaden.olvera@students.snow.edu>
2025-10-07 13:56:32 -0600

    Checkpoint 2: Greeting

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// Jaden Olvera, 10-7-25, Lab 6: Madlibs #1
-Console.WriteLine("Hello World");
+Console.Clear();
+Console.Title = "Mad Libs: Console Edition!";
+Console.WriteLine("Welcome to Mad Libs!\nWe'll take words like adjectives or nouns from you,\nthen use them to make a funny story!");
